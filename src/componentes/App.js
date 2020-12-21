82afc9896347208ebec1f5b748d37080c27428c7
Alessandro Ramos <toprime13@gmail.com>
2020-12-20 21:46:44 -0300

    adicionado dados da api

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Welcome from './Welcome';
+import Welcome from './Welcome/Welcome';
+import Dados from './API/Dados';
+import './Normalize.css';
 
-class App extends Component {
-    render() {
-        return(
-            <Router>
-                <div>
-                    <Switch>
-                        <Route exact path="/">
-                            <Welcome />
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
-            
-        )
-    }
+const App = () => {
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState([]);
+
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Dados.getHomeList();
+            setMovieList(list);
+            let originals = list.filter(i => i.slug === 'originals');
+            let randomChose = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+            let chosen = originals[0].items.results[randomChose];
+            let chosenInfo = await Dados.getMovieInfo(chosen.id, 'tv');
+            setFeaturedData(chosenInfo);
+        }
+
+        loadAll()
+    }, []);
+
+    return(
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/">
+                        <Welcome/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
