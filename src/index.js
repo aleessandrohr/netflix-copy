6309f656baf85fe83ed315b915cf274064d0937e
Alessandro Ramos <toprime13@gmail.com>
2020-12-23 22:01:25 -0300

    atualizado index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './componentes/App';
+import App from './componentes/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
