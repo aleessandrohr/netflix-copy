f494751783ee9588e9c7f311f0a33bf282a21b61
Alessandro Ramos <toprime13@gmail.com>
2020-12-24 15:27:16 -0300

    sistema de login

--- src/componentes/App/App.js
+++ src/componentes/App/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, memo } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Welcome from '../Welcome/Welcome';
+import Login from '../Login/Login';
 import Browse from '../Browse/Browse';
 import './Normalize.css';
 
 const App = () => {
+
+    const [Logged, setLogged] = useState(localStorage.getItem("Logged"));
+
+    if (Logged == null) {
+        localStorage.setItem("Logged", "false")
+    }
+
+    const LoggedTrue = (user) => {
+        localStorage.setItem("Logged", "true")
+        localStorage.setItem("Username", user)
+        setLogged("true")
+    }
+ 
     return(
         <Router>
             <div className="container">
                 <Switch>
                     <Route exact path="/">
-                        <Welcome/>
+                        {Logged === "true" ? <Redirect to="/browse" /> : <Welcome Logged={Logged} />}
+                    </Route>
+                    <Route path="/login">
+                        {Logged === "true" ? <Redirect to="/browse" /> : <Login LoggedTrue={LoggedTrue} /> }
                     </Route>
                     <Route path="/browse">
-                        <Browse />
+                        {Logged ===  "true" ? <Browse /> : <Redirect to="/" />}
                     </Route>
                 </Switch>
             </div>
@@ -21,4 +38,4 @@ const App = () => {
     )
 }
 
-export default App;
+export default memo(App);
