458edfe0a80940db4e98ded067e1aa69c958de72
Alessandro Ramos <toprime13@gmail.com>
2020-12-24 22:13:47 -0300

    melhorado função de rolagem

--- src/componentes/Browse/BrowsePage/Section/Section.js
+++ src/componentes/Browse/BrowsePage/Section/Section.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Section.css';
 
-const Section = (props) => {
+const Section = ({movie}) => {
 
     const handleLeft = (key) => {
         let movieList = document.getElementById(`movieList-${key}`)
         let style = movieList.currentStyle || window.getComputedStyle(movieList);
         let x = parseInt(style.marginLeft) + Math.round(window.innerWidth / 2);
         if (x > 0 || x === 0) {
+            let chevronLeft = document.getElementById(`Left${key}`)
+            chevronLeft.style.visibility = "hidden"
             x = 0
         }
         movieList.style.marginLeft = `${x}px`
@@ -18,19 +20,21 @@ const Section = (props) => {
         let style = movieList.currentStyle || window.getComputedStyle(movieList);
         let x = parseInt(style.marginLeft) - Math.round(window.innerWidth / 2);
         let listW = item.results.length * 150;
+        let chevronLeft = document.getElementById(`Left${key}`)
         if ((window.innerWidth - listW) > x) {
             x = (window.innerWidth - listW) - 60
         }
         movieList.style.marginLeft = `${x}px`
+        chevronLeft.style.visibility = "visible"
     }
 
-    const listItems = props.movie.map((item, key) =>
+    const listItems = movie.map((item, key) =>
         <div key={key} className="container-listItem">
             <header className="movieTitle">
                 <h1>{item.title}</h1>
             </header>
             <section className="movieChevron">
-                <div className="chevronLeft">
+                <div className="chevronLeft" id={`Left${key}`}>
                     <i className="fas fa-chevron-left" onClick={() => handleLeft(key)}></i>
                 </div>
                 <div className="chevronRight">
