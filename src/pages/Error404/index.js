db9a293819787427389e626dda0a75ddb09bdfa8
Alessandro Ramos <toprime13@gmail.com>
2020-12-26 16:37:32 -0300

    melhorado design

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavMain from '../../components/NavMain/index'
+import Footer from '../../components/Footer/index';
+import './style.css'
 
 const Error404 = () => {
     return(
@@ -7,6 +10,12 @@ const Error404 = () => {
             <header>
                 <NavMain />
             </header>
+            <section className="error404-Section">
+                <h1>Você se perdeu?</h1>
+                <p>Infelizmente não localizamos essa página. Você encontrado vários muitos <br/> outros títulos na página inicial.</p>
+                <Link className="error404-Link" to="/">Página inicial da Netflix</Link>
+            </section>
+            <Footer />
         </div>
     )
 }
