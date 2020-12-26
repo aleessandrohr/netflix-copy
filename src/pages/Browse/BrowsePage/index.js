26164eb2c833df32c462444302174baca76dd3b1
Alessandro Ramos <toprime13@gmail.com>
2020-12-26 16:36:48 -0300

    adicionado BrowsePage

---
+++ src/pages/Browse/BrowsePage/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Featured from './Featured/index'
+import Footer from '../../../components/Footer/index';
+import './style.css';
+
+const BrowsePage = ({featuredData}) => {
+
+    //header
+
+    const backgroundStyle = {
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${featuredData.backdrop_path})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
+
+    return(
+        <div className="browsePage">
+            <header style={backgroundStyle} className="browsePage-Header">
+                <Featured featuredData={featuredData} />
+            </header>
+            <section className="browsePage-Section">
+
+            </section>
+            <Footer />
+        </div>
+    );
+}
+
+export default BrowsePage;
