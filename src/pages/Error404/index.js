d176176babe999e24e9cfab2e5f21a69ffcb5695
Alessandro Ramos <toprime13@gmail.com>
2020-12-25 21:22:11 -0300

    adicionado página Error 404

---
+++ src/pages/Error404/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavMain from '../../components/NavMain/index'
+
+const Error404 = () => {
+    return(
+        <div className="error404">
+            <header>
+                <NavMain />
+            </header>
+        </div>
+    )
+}
+
+export default Error404;
