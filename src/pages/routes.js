8471dc0ddb08d562b1dcd284520b869238f8b155
Alessandro Ramos <toprime13@gmail.com>
2020-12-26 16:37:17 -0300

    adicionado nova rotas e condições

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState, memo } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Welcome from './Welcome/index';
 import Login from './Login/index';
 import Error404 from './Error404/index';
+import Browse from './Browse/index';
 
 const MainRoutes = () => {
+
+    const [isLogged,setIsLogged] = useState(localStorage.getItem("isLogged"));
+
+    const setLogged = (param) => {
+        setIsLogged(param)
+    }
+
     return(
         <Routes>
-            <Route path="/" element={localStorage.getItem("isLogged") !== "true" && <Welcome />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={isLogged !== "true" ? <Welcome /> : <Navigate to="/browse" />} />
+            <Route path="/login" element={isLogged !== "true" ? <Login setLogged={setLogged} /> : <Navigate to="/browse" />} />
+            <Route path="/browse" element={isLogged === "true" ? <Browse /> : <Navigate to="/" />} />
             <Route path="*" element={<Error404 />} />
         </Routes>
     )
 }
 
-export default MainRoutes;
+export default memo(MainRoutes);
