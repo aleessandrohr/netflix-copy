27ff8e9a3345d35471fa67822687fe7d45b090e6
Alessandro Ramos <toprime13@gmail.com>
2020-12-27 15:00:09 -0300

    alterado Link

--- src/components/NavMain/index.js
+++ src/components/NavMain/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import Netflix from '../../imagens/netflix-logo-150x79.png';
+import Netflix from '../../imagens/netflix-logo-150x46.png';
 import './style.css';
 
 const NavMain = () => {
     return(
         <nav className="navMain">
             <div className="navMain-NavLogo">
-                <Link className="navMain-NavLink" to="/"><img src={Netflix} alt="Netflix"/></Link>
+                <Link className="navMain-NavLink" to="/">
+                    <img src={Netflix} alt="Netflix"/>
+                </Link>
             </div>
         </nav>
     );
