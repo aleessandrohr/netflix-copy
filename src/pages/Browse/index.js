335350782f63174d1e2c15b69421386eaea3f06d
Alessandro Ramos <toprime13@gmail.com>
2020-12-29 14:24:47 -0300

    adicionad Outlet

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,31 +1,13 @@
-import React, { useState, useEffect, memo } from 'react';
-import BrowsePage from './BrowsePage/index';
-import BrowseLoading from './BrowseLoading/index';
-import Tmdb from '../../API/Tmdb';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Browse = () => {
 
-    const [movieList, setMovieList] = useState([]);
-    const [featuredData, setFeaturedData] = useState(null);
-
-    useEffect(() => {
-        const loadAll = async () => {
-            let list = await Tmdb.getHomeList();
-            setMovieList(list)
-            let originals = list.filter(i => i.slug === "originals");
-            let randomChose = Math.floor(Math.random() * (originals[0].items.results.length - 1));
-            let chosen = originals[0].items.results[randomChose];
-            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
-            setFeaturedData(chosenInfo);
-        }
-        loadAll()
-    }, [])
-
     return(
-        <div>
-            {featuredData ? <BrowsePage featuredData={featuredData} movieList={movieList} /> : <BrowseLoading />}
-        </div>
+        <>
+            <Outlet />
+        </>
     );
 }
 
-export default memo(Browse);
+export default Browse;
